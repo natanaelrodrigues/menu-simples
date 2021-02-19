ffc0d55a2a1f9cd0befcd379dcf6ca19850f5953
Natanael  R. Rodigues <natanaelrodrigues@gmail.com>
2021-02-19 18:45:13 -0300

    Commit Inicial

---
+++ index.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <title>Portal Modelo</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/style.css">
+  </head>
+  <body>
+      <nav> 
+        <ul class="menu">
+          <li><a href="#">Admin</a></li>
+          <li><a href="#">Cadastro</a></li>
+          <li><a href="#">Consulta</a></li>
+          <li><a href="#">Sobre</a></li>
+          <li class="submenu"><a href="#">O que fazemos?</a>
+              <ul class="item-menu">
+                      <li><a href="#">Web Design</a></li>
+                      <li><a href="#">SEO</a></li>
+                      <li class="submenu"><a href="#">Design</a>
+                        <ul>
+                          <li><a href="#" class="item-menu">Portifolio</a></li>
+                          <li><a href="#" class="item-menu">Projetos</a></li>
+                        </ul>
+                      </li>
+              </ul>
+          </li>
+		      <li><a href="#">Links</a></li>
+		      <li><a href="#">Contato</a></li>
+        </ul>
+      </nav>
+     <div class="container">
+     ...     
+     </div> 
+
+    <script src="assets/js/jquery-3.4.1.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="assets/js/bootstrap.bundle.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
