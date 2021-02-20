6e8daa08fac83f7863685e336047af74491c968d
Natanael Rodrigues <natanaelrodrigues@gmail.com>
2021-02-19 22:56:21 -0300

    inclusão do FA icon

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="assets/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/all.min.css">
     <link rel="stylesheet" href="assets/css/style.css">
   </head>
   <body>
@@ -18,7 +19,7 @@
               <ul class="item-menu">
                       <li><a href="#">Web Design</a></li>
                       <li><a href="#">SEO</a></li>
-                      <li class="submenu"><a href="#">Design</a>
+                      <li class="submenu"><a href="#">Design<i class="fas fa-angle-double-down ico"></i></a>
                         <ul>
                           <li><a href="#" class="item-menu">Portifolio</a></li>
                           <li><a href="#" class="item-menu">Projetos</a></li>
@@ -29,12 +30,13 @@
 		      <li><a href="#">Links</a></li>
 		      <li><a href="#">Contato</a></li>
           <li><a href="#">assine</a></li>
+          <li><a href="#">Assinaturas</a></li>
         </ul>
       </nav>
      <div class="container">
      ...     
      </div> 
-
      <script src="assets/js/bootstrap.bundle.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+     <script src="assets/js/all.min.js"></script>
   </body>
 </html>
